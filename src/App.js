b66f259ba563d0180c73567a7cd7e421ffcde4bb
Pierre <pierrefraisse90@gmail.com>
2023-05-08 12:01:33 +0200

    Logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.scss";
 import Modal from "./components/modal/Modal";
 import profilePic from './assets/profilepic.jpg'
+import { useState } from "react";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = (e) => {
+    if (e.target.classList.contains('handleClick')) {
+          setShowModal(!showModal);
+    }
+
+  }
+  
   return (
     <>
       <section className="intro-section --100vh --flex-center">
@@ -10,9 +20,9 @@ function App() {
           <img src={profilePic} alt="Profile Picture" />
           <h1 className="--text-light --text-md">Let's <span className="--fw-thin">Create a modal !!!</span></h1>
           <p className="--text-light --mb2">Click the button below</p>
-          <button className="--btn --btn-secondary">OPEN MODAL</button>
+          <button className="--btn --btn-secondary handleClick" onClick={handleClick}>OPEN MODAL</button>
         </div>
-        <Modal />
+        {showModal && <Modal onCloseModal={handleClick}/>}
       </section>
     </>
     
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './Modal.scss'
 import {AiOutlineClose, AiOutlineInfoCircle} from 'react-icons/ai'
 
-const Modal = () => {
+const Modal = ({ onCloseModal }) => {
   return (
-    <section className='modal-section --100vh'>
+    <section className='modal-section --100vh handleClick' onClick={onCloseModal}>
         <div className="modal --flex-center">
             <div className="modal-content --bg-light --card --p">
-                <AiOutlineClose color='red' size={16} className='close-icon' />
+                <AiOutlineClose color='red' size={16} className='close-icon handleClick' onClick={onCloseModal}/>
                 <div className="modal-head --flex-start">
                     <AiOutlineInfoCircle color='orangered' size={18}/>
                     <h3 className='--text-p --ml'>Do you want to continue ?</h3>
@@ -16,7 +16,7 @@ const Modal = () => {
                     <p className='text-sm --mb'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid amet porro cum eum incidunt, dolore est et libero, sunt fuga ea repudiandae enim totam quo alias labore nesciunt illo! Id.</p>
                 </div>
                 <div className="modal-footer --flex-end">
-                    <button className='--btn'>Cancel</button>
+                    <button className='--btn handleClick' onClick={onCloseModal}>Cancel</button>
                     <button className='--btn --btn-primary'>Proceed</button>
                 </div>
             </div>
